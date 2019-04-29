183a252de4113683d8dc3b87cdafe6db56b5668d
Sanjana Desai <sanjana.desai@shopify.com>
2019-04-29 00:13:58 -0700

    Updates

---
+++ precache-manifest.85e3d33ef18f9b3ab44c56ec78ea0dc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8947080241ed1e8d0a6f",
+    "url": "/shopify-storefront-api-learning/static/js/runtime~main.7013461b.js"
+  },
+  {
+    "revision": "15a8ef1f8d8e76c9c297",
+    "url": "/shopify-storefront-api-learning/static/js/main.f2c3faab.chunk.js"
+  },
+  {
+    "revision": "c12ea0233cef91734564",
+    "url": "/shopify-storefront-api-learning/static/js/2.15468460.chunk.js"
+  },
+  {
+    "revision": "15a8ef1f8d8e76c9c297",
+    "url": "/shopify-storefront-api-learning/static/css/main.ff6155bf.chunk.css"
+  },
+  {
+    "revision": "8759dd2d8725cbdac1915c22b260715a",
+    "url": "/shopify-storefront-api-learning/index.html"
+  }
+];
